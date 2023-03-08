0352808892642c4f45b2e8f27c068db32ae29c61
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-08 13:52:35 +0700

    add empty tag JSX

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,9 @@
 export default function Card() {
     return (
-        <div>
+        <>
             <h1>Denis</h1>
             <h3>I am studying React</h3>
             <button>Like!</button>
-        </div>
+        </>
     );
 }
